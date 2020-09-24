426e333c467f63f6b1b3575afb78af519b96ef22
Oldlynn5374 <475369943@qq.com>
2020-09-24 23:46:54 +0800

    Edition 1.8.进一步分装代码，增加了测试模块

--- SimhashProject/src/main/java/SimhashTest.java
+++ SimhashProject/src/main/java/SimhashTest.java
@@ -29,6 +29,7 @@ public class SimhashTest {
     //参与加权运算词性的权值
     private Map<String, Integer> natureWeight;
 
+
     //自定义不同词性的权值，以及是否参与加权运算
     private void setWeight() {
         //w为标点，表示标点不参与运算
@@ -59,6 +60,44 @@ public class SimhashTest {
         return content;
     }
 
+
+
+    //计算simhash值
+    public BigInteger simHash(String content) {
+        content = cleanWords(content);
+        setWeight();
+
+        //content内各关键字权值数组，用于合并运算
+        int[] array = new int[hashBits];
+
+        //把内容进行分词操作
+        List<Term> terms = StandardTokenizer.segment(content);
+
+        for (Term term : terms) {
+
+            //判断该分词是否该被过滤
+            boolean flag = wordFilter(term);
+            if (flag == true) {
+                continue;
+            }
+
+            //计算关键词的哈希值
+            BigInteger hash = hash(term.word);
+            //关键词的权值
+            Integer weight = natureWeight.get(term.nature.toString());
+            if (null == weight) {
+                weight = 1;
+            }
+
+            //加权与合并操作
+            addWeight(hash, weight, array);
+
+        }
+
+        //降维操作后返回simHash值
+        return subDimension(array);
+    }
+
     //计算哈希值
     private BigInteger hash(String source) {
 
@@ -66,9 +105,8 @@ public class SimhashTest {
             return new BigInteger("0");
         } else {
 
-            // 当sourece 的长度过短，会导致hash算法失效，因此需要对过短的词补偿
-            while (source.length()<3) {
-                source = source+source.charAt(0);
+            while (source.length() < 3) {
+                source = source + source.charAt(0);
             }
             char[] sourceArray = source.toCharArray();
             BigInteger x = BigInteger.valueOf(((long) sourceArray[0]) << 7);
@@ -86,69 +124,6 @@ public class SimhashTest {
         }
     }
 
-    //计算simhash值
-    private BigInteger simHash(String content) {
-        content = cleanWords(content);
-        setWeight();
-
-        //content内各关键字权值数组，用于合并运算
-        int[] array = new int[hashBits];
-        //把内容进行分词操作
-        List<Term> terms = StandardTokenizer.segment(content);
-        for (Term term : terms) {
-            String word = term.word;
-            String nature = term.nature.toString();
-            //过滤不参与运算的词性
-            if (this.blackNature.contains(nature)) {
-                continue;
-            }
-
-            //过滤频率过高的词
-            if (!wordCount.containsKey(word)) {
-                //第一次出现，次数为1
-                wordCount.put(word, 1);
-            } else {
-                if (wordCount.get(word) > maxCount) {
-                    //出现次数过多，跳过
-                    continue;
-                } else if ((wordCount.get(word) <= maxCount)) {
-                    //每出现一次，次数+1
-                    wordCount.put(word, wordCount.get(word) + 1);
-                }
-            }
-
-
-            //计算关键词的哈希值
-            BigInteger hash = hash(word);
-            //关键词的权值
-            Integer weight = natureWeight.get(term.nature.toString());
-            if (null == weight) {
-                weight = 1;
-            }
-
-
-            //加权、合并操作
-            for (int i = 0; i < hashBits; i++) {
-                BigInteger bit = new BigInteger("1").shiftLeft(i);
-                if (hash.and(bit).signum() == 1) {
-                    array[i] += weight;
-                } else {
-                    array[i] -= weight;
-                }
-            }
-        }
-
-        //降维操作
-        BigInteger simHashCode = new BigInteger("0");
-        for(int i =0;i<hashBits;i++){
-            if(array[i]>=0){
-                simHashCode = simHashCode.add(new BigInteger("1").shiftLeft(i));
-            }
-        }
-
-        return simHashCode;
-    }
-
     //文本对比
     public String compare(String content1, String content2) {
 
@@ -158,11 +133,59 @@ public class SimhashTest {
         BigDecimal result;
         //较小的simhash作为被除数，较大的作为除数，商即为相似度，越接近1越相似。
         if (simHash1.compareTo(simHash2) > -1) {
-            result = simHash2.divide(simHash1, 4, RoundingMode.HALF_UP);
+            result = simHash2.divide(simHash1, 4, RoundingMode.HALF_DOWN);
         } else {
-            result = simHash1.divide(simHash2, 4, RoundingMode.HALF_UP);
+            result = simHash1.divide(simHash2, 4, RoundingMode.HALF_DOWN);
         }
         return result.toString();
+    }
+
+    //判断该分词是否该被过滤。该被过滤返回true。
+    public boolean wordFilter(Term term) {
+        //过滤不参与运算的词性
+        if (this.blackNature.contains(term.nature)) {
+            return true;
+        }
+
+        //过滤频率过高的词
+        if (!wordCount.containsKey(term.word)) {
+            //第一次出现，次数为1
+            wordCount.put(term.word, 1);
+        } else {
+            if (wordCount.get(term.word) > maxCount) {
+                //出现次数过多，跳过
+                return true;
+            } else if ((wordCount.get(term.word) <= maxCount)) {
+                //每出现一次，次数+1
+                wordCount.put(term.word, wordCount.get(term.word) + 1);
+            }
+        }
+
+        return false;
+    }
+
+    //加权、合并操作
+    public void addWeight(BigInteger hash, Integer weight, int[] array) {
+
+        for (int i = 0; i < hashBits; i++) {
+            BigInteger bit = new BigInteger("1").shiftLeft(i);
+            if (hash.and(bit).signum() == 1) {
+                array[i] += weight;
+            } else {
+                array[i] -= weight;
+            }
+        }
+    }
+
+    //降维操作
+    public BigInteger subDimension(int[] array) {
+        BigInteger simHashCode = new BigInteger("0");
+        for (int i = 0; i < hashBits; i++) {
+            if (array[i] >= 0) {
+                simHashCode = simHashCode.add(new BigInteger("1").shiftLeft(i));
+            }
+        }
+        return simHashCode;
     }
 
 
